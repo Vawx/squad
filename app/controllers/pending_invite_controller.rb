213b97576008b4ac17c4d289d2e13bb20343aca9
kyle <altck8@gmail.com>
2016-04-14 07:52:24 -0700

    add notification of pending invite for user as well as thier ability to click notification and accept or decline the invite. if the accept, they are put into the project. if ignored, the pending invite is destroyed

--- app/controllers/pending_invite_controller.rb
+++ app/controllers/pending_invite_controller.rb
@@ -2,4 +2,19 @@ class PendingInviteController < ApplicationController
   define_method :index do
     @pending = PendingInvite.all.where( invite_email: current_user.email )
   end
+
+  define_method :edit do
+    pending_project = Project.find(PendingInvite.find(params[:id]).project_id)
+    if params[:format] == "join"
+      current_user.profile.projects << pending_project
+    end
+    redirect_to profile_project_path(current_user, pending_project)
+  end
+
+  define_method :destroy do
+    ignored_project_invite = PendingInvite.find(params[:id])
+    ignored_project_invite.destroy
+    ignored_project_invite.save
+    redirect_to root_path
+  end
 end
